c455dd08aaf54e85de3956596cd0f939566cec70
Sadikshya Pokharel <psaishya@gmail.com>
2023-11-22 14:53:27 +0545

    frontend of two pages completed

--- tate/tate/settings.py
+++ tate/tate/settings.py
@@ -82,26 +82,26 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
+ 
+ 
+# Password validation 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
+AUTH_PASSWORD_VALIDATORS = [ 
+    {   
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
+    }, 
+    { 
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-    {
+    { 
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+ 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
